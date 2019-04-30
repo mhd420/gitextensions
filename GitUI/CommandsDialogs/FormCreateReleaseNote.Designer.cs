7f23a261e7d8f874c03ba8fbebf8c0d8cf592052
Peter Chillemi <pchillemi@alastri.com.au>
2019-04-30 12:20:00 +0800

    mutli-line release notes

--- GitUI/CommandsDialogs/FormCreateReleaseNote.Designer.cs
+++ GitUI/CommandsDialogs/FormCreateReleaseNote.Designer.cs
@@ -35,14 +35,15 @@
             this.textNotes = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.flowProducts = new System.Windows.Forms.FlowLayoutPanel();
             this.tableLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // Ok
             // 
             this.Ok.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.Ok.DialogResult = System.Windows.Forms.DialogResult.OK;
-            this.Ok.Location = new System.Drawing.Point(364, 191);
+            this.Ok.Location = new System.Drawing.Point(396, 258);
             this.Ok.Name = "Ok";
             this.Ok.Size = new System.Drawing.Size(75, 23);
             this.Ok.TabIndex = 0;
@@ -52,41 +53,45 @@
             // 
             // textReleaseNote
             // 
-            this.textReleaseNote.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textReleaseNote.Dock = System.Windows.Forms.DockStyle.Fill;
             this.textReleaseNote.Location = new System.Drawing.Point(3, 16);
+            this.textReleaseNote.Multiline = true;
             this.textReleaseNote.Name = "textReleaseNote";
-            this.textReleaseNote.Size = new System.Drawing.Size(436, 20);
+            this.textReleaseNote.Size = new System.Drawing.Size(468, 99);
             this.textReleaseNote.TabIndex = 1;
             // 
             // tableLayoutPanel1
             // 
             this.tableLayoutPanel1.ColumnCount = 1;
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            this.tableLayoutPanel1.Controls.Add(this.Ok, 0, 4);
+            this.tableLayoutPanel1.Controls.Add(this.label3, 0, 4);
+            this.tableLayoutPanel1.Controls.Add(this.Ok, 0, 6);
             this.tableLayoutPanel1.Controls.Add(this.textReleaseNote, 0, 1);
             this.tableLayoutPanel1.Controls.Add(this.textNotes, 0, 3);
             this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
             this.tableLayoutPanel1.Controls.Add(this.label2, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.flowProducts, 0, 5);
             this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
-            this.tableLayoutPanel1.RowCount = 5;
+            this.tableLayoutPanel1.RowCount = 7;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(442, 217);
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(474, 285);
             this.tableLayoutPanel1.TabIndex = 2;
             // 
             // textNotes
             // 
             this.textNotes.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.textNotes.Location = new System.Drawing.Point(3, 55);
+            this.textNotes.Location = new System.Drawing.Point(3, 134);
             this.textNotes.Multiline = true;
             this.textNotes.Name = "textNotes";
-            this.textNotes.Size = new System.Drawing.Size(436, 130);
+            this.textNotes.Size = new System.Drawing.Size(468, 99);
             this.textNotes.TabIndex = 4;
             // 
             // label1
@@ -94,24 +99,42 @@
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(3, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(118, 13);
+            this.label1.Size = new System.Drawing.Size(72, 13);
             this.label1.TabIndex = 5;
-            this.label1.Text = "Release Note Summary";
+            this.label1.Text = "Release Note";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(3, 39);
+            this.label2.Location = new System.Drawing.Point(3, 118);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(88, 13);
             this.label2.TabIndex = 6;
             this.label2.Text = "Notes for Testing";
             // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(3, 236);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(92, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Affected Products";
+            // 
+            // flowProducts
+            // 
+            this.flowProducts.AutoSize = true;
+            this.flowProducts.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowProducts.Location = new System.Drawing.Point(3, 252);
+            this.flowProducts.Name = "flowProducts";
+            this.flowProducts.Size = new System.Drawing.Size(468, 1);
+            this.flowProducts.TabIndex = 9;
+            // 
             // FormCreateReleaseNote
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(442, 217);
+            this.ClientSize = new System.Drawing.Size(474, 285);
             this.Controls.Add(this.tableLayoutPanel1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "FormCreateReleaseNote";
@@ -130,5 +153,7 @@
         private System.Windows.Forms.TextBox textNotes;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.FlowLayoutPanel flowProducts;
     }
 }
--- GitUI/CommandsDialogs/FormCreateReleaseNote.cs
+++ GitUI/CommandsDialogs/FormCreateReleaseNote.cs
@@ -16,7 +16,10 @@ namespace GitUI.CommandsDialogs
 {
     public partial class FormCreateReleaseNote : GitExtensionsForm
     {
+        private static string[] Products = { "HI", "RR", "TS", "SC" };
+
         private readonly string _message;
+        private readonly List<CheckBox> _productCheck;
 
         public string JiraTicket { get; set; }
 
@@ -32,6 +35,20 @@ namespace GitUI.CommandsDialogs
             InitializeComplete();
 
             _message = message;
+            _productCheck = new List<CheckBox>();
+
+            foreach (var product in Products)
+            {
+                var cbox = new CheckBox()
+                {
+                    Name = $"check{product}",
+                    Text = product,
+                    AutoSize = true,
+                };
+
+                flowProducts.Controls.Add(cbox);
+                _productCheck.Add(cbox);
+            }
         }
 
         protected override void OnLoad(EventArgs e)
@@ -42,12 +59,19 @@ namespace GitUI.CommandsDialogs
 
         private void Ok_Click(object sender, EventArgs e)
         {
-            var newNote = textReleaseNote.Text;
+            var releaseNote = textReleaseNote.Text;
             var testNote = textNotes.Text;
+            var selectedProducts = _productCheck.Where(p => p.Checked).Select(p => p.Text).ToArray();
 
-            if (string.IsNullOrWhiteSpace(newNote))
+            if (string.IsNullOrWhiteSpace(releaseNote))
             {
                 MessageBox.Show("Please enter a release note.");
+                return;
+            }
+
+            if (selectedProducts.Length == 0)
+            {
+                MessageBox.Show("Please select at least one product for this release note.");
                 return;
             }
 
@@ -55,14 +79,21 @@ namespace GitUI.CommandsDialogs
             {
                 var jira = Jira.CreateRestClient(AppSettings.AlastriJiraURL, AppSettings.AlastriJiraUsername, AppSettings.AlastriJiraPassword);
 
+                var summary = releaseNote.SplitLines().FirstOrDefault();
+                var description = $"Release Notes:\r\n{releaseNote}\r\n\r\nTesting Notes:\r\n{testNote}";
+
                 var newIssue = jira.CreateIssue("RN");
                 newIssue.Type = "Task";
-                newIssue.Summary = newNote;
-                newIssue.Description = testNote;
+                newIssue.Summary = summary;
+                newIssue.Description = description;
+                newIssue["Release Notes"] = releaseNote;
+
+                newIssue.CustomFields.AddArray("Affects Projects", selectedProducts);
 
                 newIssue.SaveChanges();
 
                 JiraTicket = newIssue.Key.Value;
+                DialogResult = DialogResult.OK;
             }
             catch (Exception ex)
             {
